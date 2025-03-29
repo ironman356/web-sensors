96afc28c6946d38eeae5806a4d741f473348b58f
Alexander Held <heldalex9000@gmail.com>
2025-03-29 09:34:19 -0600

    basic cam

---
+++ misc/hardware_info.js
@@ -0,0 +1,12 @@
+
+
+document.getElementById("screenWidth").textContent = window.screen.width;
+document.getElementById("screenHeight").textContent = window.screen.height;
+document.getElementById("screenColorDepth").textContent = window.screen.colorDepth;
+document.getElementById("screenPixelDepth").textContent = window.screen.pixelDepth;
+
+document.getElementById("logicalCores").textContent = navigator.hardwareConcurrency;
+document.getElementById("memory").textContent = navigator.deviceMemory;
+document.getElementById("online").textContent = navigator.onLine ;
+
+
