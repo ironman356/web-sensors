41dff6a6a9f778f8c84366a43852f783f97ac980
Alexander Held <heldalex9000@gmail.com>
2025-06-13 20:35:37 -0600

    display level works but  euler angles aren't cooperating/at constant gimbal lock

--- gyroscope/artificial_horizon.js
+++ gyroscope/artificial_horizon.js
@@ -17,27 +17,29 @@ function toggleGyroPair() {
 }
 
 function enableOrientation() {
-    if (typeof DeviceOrientationEvent !== "undefined") {
-        if (typeof DeviceOrientationEvent.requestPermission === "function") {
-            DeviceOrientationEvent.requestPermission()
-                .then(response => {
-                    if (response === "granted") {
-                        handleOrientation();
-                        pairButtonEl.textContent = "Un-Pair";
-                    } else {
-                        pairButtonEl.textContent = "Permission Denied";
-                    }
-                })
-                .catch(err => {
-                    console.error(err);
-                    pairButtonEl.textContent = "Error";
-                });
-        } else {
-            handleOrientation();
-            pairButtonEl.textContent = "Un-Pair?";
-        }
+    if (typeof DeviceOrientationEvent.requestPermission === "function") {
+        DeviceOrientationEvent.requestPermission()
+            .then(permissionState => {
+                if (permissionState === "granted") {
+                    window.addEventListener("deviceorientation", handleOrientation);
+                    pairButtonEl.textContent = "Un-Pair";
+                } else {
+                    pairButtonEl.textContent = "Permission Denied";
+                }
+            })
+            .catch(err => {
+                // console.error(err);
+                pairButtonEl.textContent = "Error";
+            });
     } else {
-        pairButtonEl.textContent = "Not Supported";
+        try {
+            window.addEventListener("deviceorientation", handleOrientation);
+            handleOrientation();
+        } catch (error) {
+            console.log(error);
+        }
+            
+        pairButtonEl.textContent = "Un-Pair?";
     }
 }
 
@@ -47,14 +49,19 @@ function degToRad(deg) {
 
 function handleOrientation(event) {
     let pitchDeg, rollDeg;
+    console.log(typeof event);
+
     if (typeof event === "undefined") {
-        // testing data on desktop (or smth broke)
-        pitchDeg = 15;
-        rollDeg = 30;
+        pitchDeg = Math.random() * 40 - 20;
+        rollDeg = Math.random() * 40 - 20;
+        console.log(pitchDeg, rollDeg);
     } else {
+        // adjusting pitch to level at beta=90 will cause problems due to gimbal lock
+        // thank you apple for only supporting orientation in euler angles - android has options to get quaternions directly from webapi
         pitchDeg = event.beta;
         rollDeg = event.gamma;
     }
+    
 
     const cenX = canvas.width / 2;
     const cenY = canvas.width / 2; // bc is square
@@ -69,28 +76,56 @@ function handleOrientation(event) {
 
     ctx.strokeStyle = "brown";
 
+    const horizonOffset = Math.tan(degToRad(pitchDeg)) * (canvas.width / 2);
+    // console.log(horizonOffset);
 
-    const horizonOffset = Math.sin(degToRad(pitchDeg)) * canvas.width/2;
-    const yIntercept = cenY - horizonOffset;
     const slope = Math.tan(-degToRad(rollDeg));
-    const [p1, p2] = getEndPoints(yIntercept, slope, canvas.width);
+    // console.log(`slope ${slope}`);
+    const [p1, p2] = getEndPoints(horizonOffset, slope, [0, 0], canvas.width/2);
+    // console.log(horizonOffset, slope, [canvas.width/2, canvas.height/2], canvas.width/2);
+    // console.log(p1);
+    // console.log(p2);
 
-    ctx.beginPath();
-    ctx.moveTo(cenX, cenY - horizonOffset); // goto y intercept @ cenX
-    ctx.lineTo(cenX, cenY);
-    ctx.stroke();
+    if (p1 !== null && p2 !== null) {
+        ctx.beginPath();
+        ctx.moveTo(p1[0]+canvas.width/2, p1[1]+canvas.width/2);
+        ctx.lineTo(p2[0]+canvas.width/2, p2[1]+canvas.width/2);
+        
+        ctx.stroke();
+    }
+    
 
     console.log(canvas.getBoundingClientRect().width);
 }
 
-function getEndPoints(yInt, slope, boundRadius) {
-    const p1 = {x: null, y: null};
-    const p2 = {x: null, y: null};
+function getEndPoints(yInt, slope, cirMid, radius) {
+    /**
+     * y=mx+b - line formula, (x-h)^2 + (y-k)^2 = r^2 - circle formula
+     * subbing line formula into y of circle formula and rearranged gives
+     * x^2(1+m^2) + x(-2h+2mb-2mk) + (h^2+b^2+k^2-r^2-2bk) = 0
+     * from which quadratic equation can be used to find x in line formula
+     */
+    const A = 1 + slope ** 2;
+    const B = -2 * cirMid[0] + 2 * slope * yInt - 2 * slope * cirMid[1];
+    const C = cirMid[0] ** 2 + yInt ** 2 + cirMid[1] ** 2 - radius ** 2 - 2 * yInt * cirMid[1]; 
+    const inRoot = B ** 2 - 4 * A * C;
+    // console.log(inRoot)
+    if (inRoot < 0) {
+        return [null, null];
+    }
+    const x1 = (-B + Math.sqrt(inRoot)) / (2 * A);
+    const y1 = slope * x1 + yInt;
+    const x2 = (-B - Math.sqrt(inRoot)) / (2 * A);
+    const y2 = slope * x2 + yInt;
 
-    
-
-    return [p1, p2];
+    return [[x1, y1], [x2, y2]];
 }
 
 
 pairButtonEl.addEventListener("click", toggleGyroPair);
+
+
+
+function degToRad(deg) {
+    return deg * Math.PI / 180;
+}
