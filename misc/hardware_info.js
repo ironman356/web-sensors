952d9b57eef9fe71ff7710c2ceba3f10dd945607
Alexander Held <heldalex9000@gmail.com>
2025-06-17 21:26:31 -0600

    html meta update

--- misc/hardware_info.js
+++ misc/hardware_info.js
@@ -4,9 +4,10 @@ document.getElementById("screenWidth").textContent = window.screen.width;
 document.getElementById("screenHeight").textContent = window.screen.height;
 document.getElementById("screenColorDepth").textContent = window.screen.colorDepth;
 document.getElementById("screenPixelDepth").textContent = window.screen.pixelDepth;
+document.getElementById("devicePixelRatio").textContent = window.devicePixelRatio || 1;
 
 document.getElementById("logicalCores").textContent = navigator.hardwareConcurrency;
-document.getElementById("memory").textContent = navigator.deviceMemory;
-document.getElementById("online").textContent = navigator.onLine ;
+document.getElementById("memory").textContent = navigator.deviceMemory || "null";
+document.getElementById("online").textContent = navigator.onLine;
 
 
